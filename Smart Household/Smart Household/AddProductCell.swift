3ab1f5b4de430c7da5bd372ad686e8a54b0a0109
ernokanst <ernokanst@gmail.com>
2022-03-12 16:16:16 +0300

    Интеграция CocoaPods

---
+++ Smart Household/Smart Household/AddProductCell.swift
@@ -0,0 +1,23 @@
+//
+//  AddProductCell.swift
+//  Smart Household
+//
+//  Created by Дмитрий Канский on 12.03.2022.
+//
+
+import UIKit
+
+class AddProductCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Smart Household/Smart Household/ProductCell.swift
@@ -0,0 +1,22 @@
+//
+//  ProductCell.swift
+//  Smart Household
+//
+//  Created by Дмитрий Канский on 09.03.2022.
+//
+
+import UIKit
+
+class ProductCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
